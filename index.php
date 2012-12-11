64dd819b3d2272351877bea6ba0248dfc5e67ba6
Daniel <danne.r@live.se>
2012-12-11 10:47:41 +0100

    upload webbit second time

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//
+// PHASE: BOOTSTRAP
+//
+define('WEBBIT_INSTALL_PATH', dirname(__FILE__));
+define('WEBBIT_SITE_PATH', WEBBIT_INSTALL_PATH . '/site');
+
+require(WEBBIT_INSTALL_PATH.'/src/CWebbit/bootstrap.php');
+
+$ly = CWebbit::Instance();
+
+//
+// PHASE: FRONTCONTROLLER ROUTE
+//
+$ly->FrontControllerRoute();
+
+
+//
+// PHASE: THEME ENGINE RENDER
+//
+$ly->ThemeEngineRender();
+
